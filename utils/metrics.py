f7e5ce61c3bba15da2f857e1333da50b75c7b366
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-07-14 23:54:43 +0200

    feat/separate-metrics

---
+++ utils/metrics.py
@@ -0,0 +1,27 @@
+def get_metrics_from_confusion_matrix(confusion_matrix):
+    """
+    Get metrics from confusion matrix
+    :param confusion_matrix: confusion matrix
+    :return: {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1_score,
+    }
+    """
+    tp = confusion_matrix[1][1]
+    tn = confusion_matrix[0][0]
+    fp = confusion_matrix[0][1]
+    fn = confusion_matrix[1][0]
+
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1_score = 2 * (precision * recall) / (precision + recall)
+
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1_score,
+    }

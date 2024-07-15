95df94c462993e0553fe9b42c3e1b7f67e5389e2
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-07-15 23:35:26 +0200

    feat/log-evaluation-metrics-in-utils

---
+++ utils/mlflow.py
@@ -0,0 +1,17 @@
+import mlflow
+
+
+def log_evaluation_metrics(
+    dataset_type: str,
+    metrics: dict,
+    confusion_matrix,
+) -> None:
+    mlflow.log_metric(f"{dataset_type} Accuracy", metrics["accuracy"])
+    mlflow.log_metric(f"{dataset_type} Precision", metrics["precision"])
+    mlflow.log_metric(f"{dataset_type} Recall", metrics["recall"])
+    mlflow.log_metric(f"{dataset_type} F1 Score", metrics["f1_score"])
+
+    mlflow.log_metric(f"{dataset_type} True Positives", confusion_matrix[1, 1])
+    mlflow.log_metric(f"{dataset_type} False Positives", confusion_matrix[0, 1])
+    mlflow.log_metric(f"{dataset_type} False Negatives", confusion_matrix[1, 0])
+    mlflow.log_metric(f"{dataset_type} True Negatives", confusion_matrix[0, 0])

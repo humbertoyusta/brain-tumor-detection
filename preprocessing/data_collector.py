23f018e7694835ec79e09fbbda9fc0e5c9dc4ad9
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-07-17 00:08:13 +0200

    feat/store-data-collection-split-in-separate-folders

--- preprocessing/data_collector.py
+++ preprocessing/data_collector.py
@@ -2,12 +2,27 @@ import os
 import cv2
 import numpy as np
 import kaggle
+import random
+import sklearn
 from typing import Tuple
 
 
 class DataCollector:
     def __init__(self, data_root_folder: str = "data"):
         self.data_root_folder = data_root_folder
+        self.positive_images_count = {
+            "train": 0,
+            "val": 0,
+            "test": 0,
+        }
+        self.negative_images_count = {
+            "train": 0,
+            "val": 0,
+            "test": 0,
+        }
+        random.seed(0)
+        np.random.seed(0)
+        sklearn.random.seed(0)
 
     def download_data(self):
         os.makedirs(self.data_root_folder, exist_ok=True)
@@ -76,7 +91,58 @@ class DataCollector:
 
         return np.array(images), np.array(labels)
 
+    def _save_image(self, img, label, folder):
+        if label == 0:
+            img_name = self.negative_images_count[folder]
+            self.negative_images_count[folder] += 1
+            folder = os.path.join(self.data_root_folder, folder, "negative")
+        else:
+            img_name = self.positive_images_count[folder]
+            self.positive_images_count[folder] += 1
+            folder = os.path.join(self.data_root_folder, folder, "positive")
+
+        os.makedirs(folder, exist_ok=True)
+        cv2.imwrite(
+            os.path.join(folder, f"{img_name}.png"),
+            cv2.cvtColor(img, cv2.COLOR_RGB2BGR),
+        )
+
+    def split_and_store(
+        self,
+        images: np.ndarray,
+        labels: np.ndarray,
+        split_ratio: Tuple[float, float, float] = (0.75, 0.125, 0.125),
+    ):
+        train_images, remaining_images, train_labels, remaining_labels = (
+            sklearn.model_selection.train_test_split(
+                images,
+                labels,
+                test_size=split_ratio[1] + split_ratio[2],
+                random_state=0,
+            )
+        )
+
+        val_images, test_images, val_labels, test_labels = (
+            sklearn.model_selection.train_test_split(
+                remaining_images,
+                remaining_labels,
+                test_size=split_ratio[2] / (split_ratio[1] + split_ratio[2]),
+                random_state=0,
+            )
+        )
+
+        for i, (img, label) in enumerate(zip(train_images, train_labels)):
+            self._save_image(img, label, "train")
+
+        for i, (img, label) in enumerate(zip(val_images, val_labels)):
+            self._save_image(img, label, "val")
+
+        for i, (img, label) in enumerate(zip(test_images, test_labels)):
+            self._save_image(img, label, "test")
+
     def run(self, should_download=True) -> Tuple[np.ndarray, np.ndarray]:
         if should_download:
             self.download_data()
-        return self.load_images()
+        images, labels = self.load_images()
+        self.split_and_store(images, labels)
+        return images, labels

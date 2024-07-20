a6d01a556f9df1926e29cf1b458645c892033dca
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2024-07-20 18:59:13 +0200

    fefat/add-front-page

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 from app.api import api_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 app.register_blueprint(api_blueprint, url_prefix="/api/v1")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
